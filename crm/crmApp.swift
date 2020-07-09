a20251bd49493ff2eabf221ae001d76d10481e7a
iwazaki <iwazaki@uta-senzen.com>
2020-07-09 20:34:19 +0800

    Initial Commit

---
+++ crm/crmApp.swift
@@ -0,0 +1,17 @@
+//
+//  crmApp.swift
+//  crm
+//
+//  Created by iwazaki on 7/9/20.
+//
+
+import SwiftUI
+
+@main
+struct crmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
